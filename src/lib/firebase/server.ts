b2042bc555bb97c55762e4b7e36dbd52776e4eeb
Taimiya Amjad <taimiyaamjad0@gmail.com>
2025-09-28 08:17:44 +0000

    Now what is this?  (_for element <Primitive.li>_)

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -4,20 +4,12 @@ import { getApp as getAdminApp, getApps as getAdminApps, initializeApp as initia
 import { getFirestore as getAdminFirestore } from 'firebase-admin/firestore';
 import { credential } from "firebase-admin";
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY
-  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
-  : undefined;
-
 let adminApp: App;
+
 if (!getAdminApps().length) {
-  if (serviceAccount) {
-    adminApp = initializeAdminApp({
-      credential: credential.cert(serviceAccount),
-    });
-  } else {
-    // This is for local development without service account credentials
-    adminApp = initializeAdminApp();
-  }
+  // When deployed to a Google Cloud environment, the Admin SDK is automatically
+  // initialized with the default credentials of the service account.
+  adminApp = initializeAdminApp();
 } else {
   adminApp = getAdminApp();
 }
